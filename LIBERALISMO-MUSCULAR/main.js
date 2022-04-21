262da6164660b6ae909f105d03c0448ba46bec44
speedbuild98 <lautaropectm98@gmail.com>
2022-04-21 12:19:48 -0300

    Agregué botón scrollup

--- LIBERALISMO-MUSCULAR/main.js
+++ LIBERALISMO-MUSCULAR/main.js
@@ -82,4 +82,11 @@ function openNav() {
       document.getElementById("mySidenav").style.width = "0";
   }
 
-  
+/*==========================================SCROLL UP==========================================*/
+
+  function scrollUp(){
+const scrollUp = document.getElementById('scroll-up');
+// When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+if(this.scrollY >= 400) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+}
+window.addEventListener('scroll', scrollUp)
